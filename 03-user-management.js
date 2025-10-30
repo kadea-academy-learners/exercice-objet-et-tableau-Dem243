bcf390f678bdcae8134866c0cc5aef8902962ee7
dem243 <raphaelmutombo707@gmail.com>
2025-10-30 21:06:12 +0100

    feat: solution to the third exercise

--- 03-user-management.js
+++ 03-user-management.js
@@ -5,12 +5,25 @@
 // - estAdmin (boolean)
 // La fonction doit retourner un tableau contenant les noms des utilisateurs qui sont admin.
 
-function whoIsAdmin() {
-	
-}
+function whoIsAdmin(users) {
+	let result = [];
 
+	for (let user of users) {
+		if (user.estAdmin) {
+			result.push(user.nom);
+		}
+	}
+
+	return result;
+}
+const users = [
+  { nom: "Raph", age: 28, estAdmin: true },
+  { nom: "Lionel", age: 20, estAdmin: false },
+  { nom: "Charlie", age: 22, estAdmin: true }
+];
+
+
+console.log(whoIsAdmin(users));
 module.exports = {
 	whoIsAdmin,
 };
-
-
